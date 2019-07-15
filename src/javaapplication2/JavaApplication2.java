71d4ff954dce2f165d7344625eca1ae71c2d59eb
usager <usager@10.0.2.15>
2019-07-15 09:55:26 -0400

    ngrok ajouter merici

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -18,6 +18,7 @@ public class JavaApplication2 {
         System.out.println("Bonjour "+args[1]+", " +args[0]);
         
         System.out.println("Au revoir!");
+        System.out.println("Merici!");
     }
     
 }
