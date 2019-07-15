1f891ea1de44df6626f580bd98540ea4257f949d
usager <usager@10.0.2.15>
2019-07-15 09:17:33 -0400

    ngrok ajouter au revoir

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -16,6 +16,8 @@ public class JavaApplication2 {
      */
     public static void main(String[] args) {
         System.out.println("Bonjour "+args[1]+", " +args[0]);
+        
+        System.out.println("Au revoir!");
     }
     
 }
